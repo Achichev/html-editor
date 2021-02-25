ecdd767444902f5a4223e6bfece29af5945f794d
Alexey <69649787+Achichev@users.noreply.github.com>
2021-02-25 19:13:59 +0300

    Update UndoAction.java

--- actions/UndoAction.java
+++ actions/UndoAction.java
@@ -1,6 +1,6 @@
-package com.javarush.task.task32.task3209.actions;
+package html_editor.actions;
 
-import com.javarush.task.task32.task3209.View;
+import html_editor.View;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
